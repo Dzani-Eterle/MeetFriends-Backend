87acbbf4a332c81f68745dddb6c99a431f6b04e7
dzani <dzanieterle531@gmail.com>
2024-06-12 15:55:23 +0200

    pusghed older version to be able to present working version of project

---
+++ Meet-Friends-BE/src/main/java/com/MeetFriendsBE/DTOs/UserDTO.java
@@ -0,0 +1,18 @@
+// src/main/java/com/MeetFriendsBE/DTOs/UserDTO.java
+package com.MeetFriendsBE.DTOs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class UserDTO {
+    private long id;
+    private String username;
+    private String password;
+    private String email;
+    private List<UserDTO> friendlist;
+
+}
